26e51abaf0132b221f3ee40def678efb3d7d78f4
sumnii <sumsong@student.42seoul.kr>
2023-01-26 16:30:11 +0900

    [TEST] 2 cases with exec command and without

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -11,5 +11,8 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWD';
 FLUSH PRIVILEGES;
 eof
 
-# bash
+# 그냥 mysql server 실행
 mysqld
+
+# 현재 쉘스크립트를 실행중인 bash(pid 1)를 mysqld로 대체
+# exec mysqld
