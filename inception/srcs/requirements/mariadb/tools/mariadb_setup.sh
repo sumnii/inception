0a5aeeb65b941122e97d00621398e01b09871a4d
sumnii <sumsong@student.42seoul.kr>
2023-01-26 10:22:13 +0900

    [FIX] sql command in bash file for using env

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 service mysql start
-mysql < db_setting.sql
+
+# db_setting
+mysql <<eof
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWD';
+FLUSH PRIVILEGES;
+eof
 
 # check
 echo hi
