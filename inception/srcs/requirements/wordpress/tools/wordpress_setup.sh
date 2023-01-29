0ee80b5a46ebb96a4469f59e23a3873440a3cbe3
sumnii <sumsong@student.42seoul.kr>
2023-01-29 20:22:34 +0900

    [FEAT] set up wordpress using wp-cli

--- inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-service php7.3-fpm start
+# nginx 관리자에 wp가 담길 디렉토리에 대한 권한 할당
+chown -R www-data:www-data /var/www/
 
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz
-mv wordpress/ var/www/html/
-chown -R www-data:www-data /var/www/html/wordpress
-mv ./wp-config.php var/www/html/wordpress/wp-config.php 
+if [ ! -f "/var/www/html/wordpress/index.php" ]; then
+	sudo -u www-data sh -c " \
+	echo "hello!" && \
+	wp core download --locale=$WORDPRESS_LANG && \
+	wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USER \
+	--dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" && \
+	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE \
+	--admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD \
+	--admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email && \
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD && \
+	wp plugin update --all
+	"
+fi
 
-bash
+# foreground로 실행
+exec php-fpm7.3 -F
