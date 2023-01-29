e179912fe091457a3ea9e86a19eb94d862f4ebac
sumnii <sumsong@student.42seoul.kr>
2023-01-30 01:56:27 +0900

    [FIX] rm resetting volumes

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-service mysql start
+# 초기 설정 확인
+cat /var/lib/mysql/.setup 2> /dev/null
 
 # db_setting
-mysql -uroot -p$MYSQL_ROOT_PASSWORD <<eof
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-eof
+if [ $? -ne 0 ]; then
+	mysqld_safe
 
-mysql $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
-mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+	mysql -e "FLUSH PRIVILEGES;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+	touch /var/lib/mysql/.setup
+
+	mysql $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
+	mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+fi
 
 # 현재 쉘스크립트를 실행중인 bash(pid 1)를 대체
 exec mysqld_safe
