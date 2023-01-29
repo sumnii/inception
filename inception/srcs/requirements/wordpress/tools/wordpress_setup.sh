a3eb75ce4279d954f9bb166a3731d5972107bc8b
sumnii <sumsong@student.42seoul.kr>
2023-01-30 01:08:12 +0900

    [FEAT] mySQL server test

--- inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# nginx 관리자에 wp가 담길 디렉토리에 대한 권한 할당
+# nginx 관리자(www-data)에 wp가 담길 디렉토리에 대한 권한 할당
 chown -R www-data:www-data /var/www/
+
+if ! mysqladmin -h $WORDPRESS_DB_HOST -u $MYSQL_USER --password=$MYSQL_PASSWORD --wait=60 ping > /dev/null; then
+	printf "MariaDB Daemon Unreachable\n"
+	exit 1
+fi
 
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	sudo -u www-data sh -c " \
-	echo "hello!" && \
 	wp core download --locale=$WORDPRESS_LANG && \
-	wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USER \
+
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$WORDPRESS_DB_USER \
 	--dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" && \
+	
 	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE \
 	--admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD \
 	--admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email && \
+
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD && \
+	
 	wp plugin update --all
 	"
 fi
