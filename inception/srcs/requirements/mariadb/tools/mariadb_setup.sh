7fad802d22e063592196d43b291bce4f14760929
sumnii <sumsong@student.42seoul.kr>
2023-01-31 15:29:29 +0900

    [FEAT] soft symbolic link

--- inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -5,19 +5,22 @@ cat /var/lib/mysql/.setup 2> /dev/null
 
 # db_setting
 if [ $? -ne 0 ]; then
-	mysqld_safe
+	service mysql start
 
 	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 	mysql -e "FLUSH PRIVILEGES;"
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-	touch /var/lib/mysql/.setup
-
 	mysql $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
 	mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+	
+	touch /var/lib/mysql/.setup
+
+	ln -s /run/mysqld/mysqld.sock /var/lib/mysql/mysql.sock
 fi
+
 
 # 현재 쉘스크립트를 실행중인 bash(pid 1)를 대체
 exec mysqld_safe
