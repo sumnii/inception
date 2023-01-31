a9d2a1a4d11f42b4b20bd66b4c3ab8aa51f533e6
sumnii <sumsong@student.42seoul.kr>
2023-01-31 15:59:37 +0900

    [FIX] wordpress_setup.sh

--- inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # nginx 관리자(www-data)에 wp가 담길 디렉토리에 대한 권한 할당
+sleep 5
 chown -R www-data:www-data /var/www/
-
-# if ! mysqladmin --host=$WORDPRESS_DB_HOST --port=3306 --user=$MYSQL_USER --password=$MYSQL_PASSWORD --wait=30 ping > /dev/null; then
-# 	printf "MariaDB Daemon Unreachable\n"
-# 	exit 1
-# fi
 
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	sudo -u www-data sh -c " \
@@ -22,9 +18,8 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD && \
 	
 	wp plugin update --all
-
-	echo "127.0.0.1 $DOMAIN_NAME" >> /etc/hosts
 	"
+	sudo echo "127.0.0.1 $DOMAIN_NAME" >> /etc/hosts
 fi
 
 # foreground로 실행
